8330bf7dbb4adfde864746f411907b1be7c2fa23
Artemiy Misyurov <14artemiy88@gmail.com>
2023-07-09 20:03:26 +0500

    menu fix

--- menu.go
+++ menu.go
@@ -9,17 +9,18 @@ const menuWidth = 15
 const helpWidth = 37
 
 var symbols = map[string]pixel{
-	"3,1": {X: 3, Y: 1, symbol: "."}, "5,1": {X: 5, Y: 1, symbol: ":"}, "7,1": {X: 7, Y: 1, symbol: "!"}, "9,1": {X: 9, Y: 1, symbol: "/"},
-	"3,3": {X: 3, Y: 3, symbol: "r"}, "5,3": {X: 5, Y: 3, symbol: "("}, "7,3": {X: 7, Y: 3, symbol: "l"}, "9,3": {X: 9, Y: 3, symbol: "1"},
-	"3,5": {X: 3, Y: 5, symbol: "Z"}, "5,5": {X: 5, Y: 5, symbol: "4"}, "7,5": {X: 7, Y: 5, symbol: "H"}, "9,5": {X: 9, Y: 5, symbol: "9"},
-	"3,7": {X: 3, Y: 7, symbol: "W"}, "5,7": {X: 5, Y: 7, symbol: "8"}, "7,7": {X: 7, Y: 7, symbol: "$"}, "9,7": {X: 9, Y: 7, symbol: "@"},
-	"3,9": {X: 3, Y: 9, symbol: "░"}, "5,9": {X: 5, Y: 9, symbol: "▒"}, "7,9": {X: 7, Y: 9, symbol: "▓"}, "9,9": {X: 9, Y: 9, symbol: "█"},
+	"3,1": {X: 3, Y: 1, symbol: "┌"}, "5,1": {X: 5, Y: 1, symbol: "├"}, "7,1": {X: 7, Y: 1, symbol: "┬"}, "9,1": {X: 9, Y: 1, symbol: "─"}, "11,1": {X: 11, Y: 1, symbol: "┐"},
+	"3,3": {X: 3, Y: 3, symbol: "└"}, "5,3": {X: 5, Y: 3, symbol: "┤"}, "7,3": {X: 7, Y: 3, symbol: "┴"}, "9,3": {X: 9, Y: 3, symbol: "│"}, "11,3": {X: 11, Y: 3, symbol: "┘"}, "13,3": {X: 13, Y: 3, symbol: "┼"},
+	"3,5": {X: 3, Y: 5, symbol: "┏"}, "5,5": {X: 5, Y: 5, symbol: "┣"}, "7,5": {X: 7, Y: 5, symbol: "┳"}, "9,5": {X: 9, Y: 5, symbol: "━"}, "11,5": {X: 11, Y: 5, symbol: "┓"},
+	"3,7": {X: 3, Y: 7, symbol: "┗"}, "5,7": {X: 5, Y: 7, symbol: "┫"}, "7,7": {X: 7, Y: 7, symbol: "┻"}, "9,7": {X: 9, Y: 7, symbol: "┃"}, "11,7": {X: 11, Y: 7, symbol: "┛"}, "13,7": {X: 13, Y: 7, symbol: "╋"},
+	"3,9": {X: 3, Y: 9, symbol: "╭"}, "5,9": {X: 5, Y: 9, symbol: "╮"}, "7,9": {X: 7, Y: 9, symbol: "╯"}, "9,9": {X: 9, Y: 9, symbol: "╰"},
+	"3,11": {X: 3, Y: 11, symbol: "░"}, "5,15": {X: 5, Y: 11, symbol: "▒"}, "7,15": {X: 7, Y: 11, symbol: "▓"}, "9,15": {X: 9, Y: 11, symbol: "█"},
 }
 
 var colors = map[string]pixel{
-	"3,13": {X: 3, Y: 13, symbol: "R"},
-	"3,15": {X: 3, Y: 15, symbol: "G"},
-	"3,17": {X: 3, Y: 17, symbol: "B"},
+	"3,15": {X: 3, Y: 15, symbol: "R"},
+	"3,17": {X: 3, Y: 17, symbol: "G"},
+	"3,19": {X: 3, Y: 19, symbol: "B"},
 }
 
 type menuItem struct {
@@ -102,7 +103,7 @@ func drawSymbolMenu(s screen, board [][]string) [][]string {
 
 func drawColorMenu(s screen, board [][]string) [][]string {
 	str := "Color " + strings.Repeat("─", menuWidth-2-len("Color ")) + "┤"
-	drawString(0, 11, str, board)
+	drawString(0, 13, str, board)
 	for _, c := range colors {
 		switch c.symbol {
 		case "R":
@@ -117,12 +118,12 @@ func drawColorMenu(s screen, board [][]string) [][]string {
 		}
 
 	}
-	board[19][3] = fgRgb(s.color.R, s.color.G, s.color.B, "█")
-	board[19][4] = fgRgb(s.color.R, s.color.G, s.color.B, "█")
-	board[19][5] = fgRgb(s.color.R, s.color.G, s.color.B, "█")
-	board[20][3] = fgRgb(s.color.R, s.color.G, s.color.B, "█")
-	board[20][4] = fgRgb(s.color.R, s.color.G, s.color.B, "█")
-	board[20][5] = fgRgb(s.color.R, s.color.G, s.color.B, "█")
+	board[21][3] = fgRgb(s.color.R, s.color.G, s.color.B, "█")
+	board[21][4] = fgRgb(s.color.R, s.color.G, s.color.B, "█")
+	board[21][5] = fgRgb(s.color.R, s.color.G, s.color.B, "█")
+	board[22][3] = fgRgb(s.color.R, s.color.G, s.color.B, "█")
+	board[22][4] = fgRgb(s.color.R, s.color.G, s.color.B, "█")
+	board[22][5] = fgRgb(s.color.R, s.color.G, s.color.B, "█")
 
 	return board
 }
