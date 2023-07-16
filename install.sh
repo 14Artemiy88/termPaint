d0e73c0d296cfe0341cb09937e1cfc74b21fd19a
Artemiy Misyurov <14artemiy88@gmail.com>
2023-07-16 21:00:14 +0500

    ref

--- install.sh
+++ install.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-git clone https://github.com/14Artemiy88/termPaint
-cd termPaint || exit
-go install .
+go install github.com/14Artemiy88/termPaint@latest
 conf_dir="$HOME/.config/termPaint"
-if [[ ! -d $conf_dir ]]; then
+if [[ ! -d "$conf_dir" ]]; then
     mkdir $conf_dir
 fi
 if [[ ! -f "$conf_dir/config.yaml" ]]; then
-    cp config.yaml "$conf_dir"
+    curl -O https://raw.githubusercontent.com/14Artemiy88/termPaint/main/config.yaml "$conf_dir"
 fi
-cd ..
-yes | rm -r termPaint
