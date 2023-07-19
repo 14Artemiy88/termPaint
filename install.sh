c229f996e08fbbadc804b09594a26ccf95076164
Artemiy Misyurov <14artemiy88@gmail.com>
2023-07-19 16:20:54 +0500

    fix installer

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ if [[ ! -d "$conf_dir" ]]; then
     mkdir "$conf_dir"
 fi
 file="$conf_dir/config.yaml"
-if [[ ! -f "$file" ]]; then
+if [[ -f "$file" ]]; then
     echo mv "$file" "$file.old"
 fi
 curl -o "$file"  https://raw.githubusercontent.com/14Artemiy88/termPaint/main/config.yaml
