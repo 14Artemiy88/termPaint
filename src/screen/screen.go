6bc94e143a4e548d65ae697a31b91a94f51bd288
Artemiy <misyurov_aa@ekb.sima-land.ru>
2025-06-02 23:27:43 +0500

    linter fixes

--- src/screen/screen.go
+++ src/screen/screen.go
@@ -218,7 +218,7 @@ func (s *Screen) drawScreen() {
 
 func (s *Screen) drawClearScreen() {
 	s.SavedPixels = make([][]string, s.Height)
-	for i := range s.Height {
+	for i := 0; i < s.Height; i++ {
 		if len(s.SavedPixels) > i {
 			s.SavedPixels[i] = strings.Split(strings.Repeat(" ", s.Width), "")
 		}
